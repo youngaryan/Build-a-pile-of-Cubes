72bcce1fd05375d60d3b24261e41401ae9c4598c
Aryan Golbaghi <aryangolbaghi@gmail.com>
2023-02-15 11:40:27 +0000

    i have coded the main part of the program howeve the program for big numbers such as 4183059834009L is extremely slow

--- cube.java
+++ cube.java
@@ -3,5 +3,25 @@
  */
 public class cube {
 
-    
+    public static void main(String[] args) {
+
+        System.out.println(findNb(4183059834009L));
+        }
+    public static long findNb(long m) {
+        int n = 0;
+        int sum =0;
+        
+
+        while (sum<m) {
+            n +=1;
+           int a = (n*n+n);
+           int p = 2;
+           sum = (int) (Math.pow(a, p)/4);
+           System.out.println(n);
+        }
+
+        return n;
+        
+	}	
+
 }
