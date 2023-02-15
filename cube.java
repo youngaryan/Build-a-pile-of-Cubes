9b9b03c657eaa47d2173b0e42a5c2b45270c40c9
Aryan Golbaghi <aryangolbaghi@gmail.com>
2023-02-15 11:57:11 +0000

    i changed the cast for a and sum to long and now the program works quicker

--- cube.java
+++ cube.java
@@ -9,18 +9,22 @@ public class cube {
         }
     public static long findNb(long m) {
         int n = 0;
-        int sum =0;
+        long sum =0;
         
 
         while (sum<m) {
             n +=1;
-           int a = (n*n+n);
+           Long a = (long) (n*n+n);
            int p = 2;
-           sum = (int) (Math.pow(a, p)/4);
-           System.out.println(n);
+           sum = (long) (Math.pow(a, p)/4);
         }
+        if (sum == m) {
+            return n;
+            
+        } else {
+            return -1;
 
-        return n;
+        }
         
 	}	
 
